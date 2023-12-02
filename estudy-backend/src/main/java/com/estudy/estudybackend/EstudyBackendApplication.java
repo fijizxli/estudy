51809de146ba68bf821258da5e59f6b6ba23f112
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-12-02 07:18:04 +0100

    Initialize backend.

---
+++ estudy-backend/src/main/java/com/estudy/estudybackend/EstudyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estudy.estudybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudyBackendApplication.class, args);
+    }
+
+}
