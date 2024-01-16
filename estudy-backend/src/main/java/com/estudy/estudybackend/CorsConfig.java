70d1406ced94eb4cde943e6d19aacc04dd449740
Zoltan Szabo <szabozoltan6x@gmail.com>
2024-01-16 17:18:15 +0100

    Update CorsConfig

--- estudy-backend/src/main/java/com/estudy/estudybackend/CorsConfig.java
+++ estudy-backend/src/main/java/com/estudy/estudybackend/CorsConfig.java
@@ -1,10 +1,9 @@
 package com.estudy.estudybackend;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/*
 
 @Configuration
 public class CorsConfig {
@@ -23,3 +22,14 @@ public class CorsConfig {
         return new CorsFilter(source);
     }
 }
+*/
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
+                
