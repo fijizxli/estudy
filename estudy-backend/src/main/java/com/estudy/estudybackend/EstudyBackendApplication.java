21750a5f506f7464c6b970b62ad8739944174ca8
Zoltan Szabo <szabozoltan6x@gmail.com>
2024-01-16 17:18:34 +0100

    Update EstudyBackendApplication

--- estudy-backend/src/main/java/com/estudy/estudybackend/EstudyBackendApplication.java
+++ estudy-backend/src/main/java/com/estudy/estudybackend/EstudyBackendApplication.java
@@ -1,9 +1,7 @@
 package com.estudy.estudybackend;
 
-import com.estudy.estudybackend.models.Role;
-import com.estudy.estudybackend.models.User;
-import com.estudy.estudybackend.repositories.RoleRepository;
-import com.estudy.estudybackend.repositories.UserRepository;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Set;
+import com.estudy.estudybackend.models.Role;
+import com.estudy.estudybackend.models.User;
+import com.estudy.estudybackend.repositories.RoleRepository;
+import com.estudy.estudybackend.repositories.UserRepository;
 
 @SpringBootApplication
 public class EstudyBackendApplication {
@@ -20,24 +21,29 @@ public class EstudyBackendApplication {
         SpringApplication.run(EstudyBackendApplication.class, args);
     }
 
-    /*
     @Autowired
     RoleRepository roleRepository;
 
     @Bean
     public CommandLineRunner dataLoader(UserRepository userRepository, PasswordEncoder encoder){
-        Role adminRole = new Role("ADMIN");
-        Role userRole = new Role("USER");
-        roleRepository.save(adminRole);
-        roleRepository.save(userRole);
+
+        if (roleRepository.findByName("ADMIN") == null){
+            Role adminRole = new Role("ADMIN");
+            roleRepository.save(adminRole);
+        }
+
+        if (roleRepository.findByName("USER") == null){
+            Role userRole = new Role("USER");
+            roleRepository.save(userRole);
+        }
 
         return args -> {
-            User admin = new User("admin", encoder.encode("admin"), Set.of(adminRole));
-            userRepository.save(admin);
-
+            if (userRepository.findByUsername("HUV7eQ3XjDX5zw") == null){
+                User admin = new User("HUV7eQ3XjDX5zw", 
+                encoder.encode("71Va3w0Sjyg1BTk6yeDSQd8SEMxveX0Nf0WS6BcZy8eUPtmfcu"),
+                Set.of(roleRepository.findByName("admin")));
+                userRepository.save(admin);
+            }
         };
     }
-
-     */
-
 }
