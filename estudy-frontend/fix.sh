215af77cc48188cc799f34507c0bcf78a33fe27d
Zoltan Szabo <szabozoltan6x@gmail.com>
2024-01-27 09:24:36 +0100

    Initialize new frontend with vite, react and tailwind

--- estudy-frontend/fix.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ulimit -n 8096
--- estudy-frontend/src/components/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ErrorMessage(props) {
-    return (
-        <div className="errorMessage">{props.text}</div>
-    )
-}
-
-export default ErrorMessage
