a941453506b4e19662bc72d9a3d20866865957c5
Szabó Zoltán <szabozoltan6x@gmail.com>
2024-02-10 15:00:19 +0100

    Remove unused imports

--- estudy-backend/src/main/java/com/estudy/estudybackend/models/StudyMaterial.java
+++ estudy-backend/src/main/java/com/estudy/estudybackend/models/StudyMaterial.java
@@ -1,10 +1,7 @@
 package com.estudy.estudybackend.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-
-import java.util.List;
 
 @Entity
 @Table(name="study_material")
