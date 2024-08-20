309e07c4ce5f3250b4894462d6718826cf451079
Zoltán Szabó <szabozoltan6x@gmail.com>
2024-08-20 13:20:39 +0200

    feat: create profile page

--- estudy-frontend/src/components/Profile.tsx
+++ estudy-frontend/src/components/Profile.tsx
@@ -5,12 +5,11 @@ import {useAuth} from "../context";
 import {Card, CardHeader, CardContent, CardTitle, CardDescription } from "./ui/card";
 import { Label } from "./ui/label";
 import { Course } from '../types';
+import { PersonIcon } from "@radix-ui/react-icons";
 
 export default function MyCourses() {
     const { user } = useAuth(); 
     const [courses, setCourses] = useState([]);
-
-    console.log(user?.username)
 
     useEffect(()=> {
             axios.get('/' + user?.username + '/courses', {
@@ -24,7 +23,30 @@ export default function MyCourses() {
 
     return user?.isLoggedIn ? (
         <div className="m-auto pt-10">
-            <Label className="text-3xl flex m-auto pt-20 pb-10 justify-center"><b>My Courses</b></Label>
+            <Label className="text-3xl flex m-auto pt-20 justify-center"><PersonIcon className="mr-2 h-9 w-9"/><b>Profile</b></Label>
+            <Label className="text-2xl lg:text-3xl flex mb-auto pt-10 pl-20"><b>Profile details:</b></Label>
+            <div className=" grid grid-cols-1 lg:w-11/12 xl:w-9/12 m-auto">
+                <Card className="flex flex-col m-auto p-auto mt-4 mb-4 p-2 w-80 break-words mb hover:bg-slate-200">
+                    <CardHeader>
+                    <CardTitle className="hover:underline">Username: {user.username}</CardTitle>
+                    <CardDescription>
+
+                    </CardDescription>
+                    </CardHeader>
+                    <CardContent className=" h-full">
+                    <ul>
+                        <li>Email address: {user.emailAddress}</li>
+                        <li>Role: {user.role}</li>
+                    </ul>
+                    </CardContent>
+                    {/* 
+                    <CardFooter>
+                        <Button className="w-80">Join</Button>
+                    </CardFooter>
+                    */}
+                </Card>
+            </div>
+            <Label className="text-2xl lg:text-3xl flex mb-auto pt-10 pl-20"><b>My courses:</b></Label>
             <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:w-11/12 xl:w-9/12 m-auto">
             {courses.map((course: Course) => (
                 <Card key={course.id} className="flex flex-col m-auto p-auto mt-4 mb-4 p-2 w-80 break-words mb hover:bg-slate-200">
