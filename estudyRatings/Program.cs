332feb8cb57c502d0c22d8df98171c08ece085a2
Szabó Zoltán <szabozoltan6x@gmail.com>
2024-09-12 14:23:42 +0200

    add mongodb connection to ratings site

--- estudyRatings/Program.cs
+++ estudyRatings/Program.cs
@@ -1,4 +1,21 @@
 using estudyRatings.Components;
+using estudyRatings.Components.Data;
+using MongoDB.Driver;
+
+//var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+var connectionString = 
+    Environment.GetEnvironmentVariable("mongodb://root:example@localhost:27017/ratings?authSource=admin");
+if (connectionString == null)
+{
+    Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, " +
+                      "see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string"
+                      );
+    Environment.Exit(0);
+}
+var client = new MongoClient(connectionString);
+var db = LecturerDBContext.Create(client.GetDatabase("lecturers"));
+
+db.Database.EnsureCreated();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +29,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios,
+    // see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
