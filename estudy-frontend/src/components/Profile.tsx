5f2c787f71b6dc6aef28cb3a06cbd4696f528802
Szabó Zoltán <szabozoltan6x@gmail.com>
2024-10-09 17:52:05 +0200

    add avatar + editing to profile page

--- estudy-frontend/src/components/Profile.tsx
+++ estudy-frontend/src/components/Profile.tsx
@@ -5,14 +5,20 @@ import {useAuth} from "../context";
 import {Card, CardHeader, CardContent, CardTitle, CardDescription } from "./ui/card";
 import { Label } from "./ui/label";
 import { Course } from '../types';
-import { PersonIcon } from "@radix-ui/react-icons";
+import { Pencil2Icon, PersonIcon } from "@radix-ui/react-icons";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 export default function MyCourses() {
     const { user } = useAuth(); 
     const [courses, setCourses] = useState([]);
     const [lecturerCourses, setLecturerCourses] = useState([]);
     const [avatarPath, setAvatarPath] = useState<string>("");
+    const [username, setUsername] = useState("");
+    const [emailAddress, setEmailAddress] = useState("");
+    const [file, setFile] = useState<File | null>(null);
 
     useEffect(()=> {
             axios.get('/' + user?.username + '/courses', {
@@ -30,7 +36,6 @@ export default function MyCourses() {
                             'Content-Type': 'application/json',
                         }}).then(function (response) {
                             setLecturerCourses(response.data)
-                            console.log(response.data)
                         })
                     }
             },[]) ;
@@ -41,23 +46,86 @@ export default function MyCourses() {
         );
     }
 
+    const handleEdit = () => {
+        console.log("TODO: IMPLEMENTATION")
+    };
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+
     return user?.isLoggedIn ? (
         <div className="m-auto pt-10">
-            <Label className="text-3xl flex m-auto pt-20 pb-4 justify-center"><b>Profile</b></Label>
-            <Avatar className="w-40 h-40 m-auto p-auto border-solid border-2 border-black justify-center">
-                <AvatarImage src={avatarPath}/>
-                <AvatarFallback><PersonIcon/></AvatarFallback>
-            </Avatar>
-            <Label className="text-2xl lg:text-3xl flex mb-auto pt-10 pl-20"><b>Profile details:</b></Label>
-            <div className=" grid grid-cols-1 lg:w-11/12 xl:w-9/12 m-auto">
-                <Card className="flex flex-col m-auto w-mt-4 mb-4 p-4 break-words mb hover:bg-slate-200">
-                    <CardHeader>
-                    <CardTitle className="hover:underline">Username: {user.username}</CardTitle>
-                    <CardDescription>
+            <Label className="text-3xl flex m-auto pt-20 pb-4 justify-center"><b>{user.username}</b></Label>
 
+            <div className="sm:w-max md:w-max md:w-full lg:w-11/12 xl:w-9/12 m-auto">
+
+                <Avatar className="flex flex-col w-40 h-40 m-auto border-solid border-2 border-black">
+                    <AvatarImage src={avatarPath}/>
+                    <AvatarFallback><PersonIcon className="w-40 h-40"/></AvatarFallback>
+                </Avatar>
+                <Card className="w-80 flex flex-col break-words m-auto mt-4 hover:bg-slate-200 border-solid border-2 border-black">
+                    <CardHeader>
+
+                    <Dialog>
+                        <DialogTrigger className="mr-auto">
+                            <CardTitle className="hover:underline inline-block">{user.username}
+                                <Pencil2Icon className="inline-block"/>
+                            </CardTitle>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Edit user details of <i>{user.username}</i></DialogTitle>
+                                <DialogDescription>
+                                </DialogDescription>
+                            </DialogHeader>
+                            <form onSubmit={handleEdit}>
+                                <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <Label htmlFor="username">
+                                    <b>username</b>
+                                </Label>
+                                <Input
+                                    className="w-100 pu-0 mb-2 mu-4 inline-block radius-10 box"
+                                    type="text"
+                                    placeholder="username"
+                                    id="username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
+                                />
+                                </div>
+                                <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <Label htmlFor="emailAddress">
+                                    <b>email address: </b>
+                                </Label>
+                                <Input
+                                    className="w-100 pu-0 mb-2 mu-4 inline-block radius-10 box"
+                                    type="text"
+                                    placeholder="emailaddress@example.com"
+                                    id="emailAddress"
+                                    value={emailAddress}
+                                    onChange={(e) => setEmailAddress(e.target.value)}
+                                    required
+                                />
+                                </div>
+                                <div className="grid max-w-sm items-center gap-1. 5w-100 pu-0 mb-2 mu-8 inline-block radius-10 box">
+                                <Label htmlFor="picture"><b>Profile picture</b></Label>
+                                <Input id="file" type="file" onChange={handleFileChange} />
+                                </div>
+                                <Button type="submit">
+                                    Confirm
+                                </Button>
+                        </form>
+                        </DialogContent>
+                    </Dialog>
+
+                    <CardDescription>
+                        User details
                     </CardDescription>
                     </CardHeader>
-                    <CardContent className=" h-full">
+                    <CardContent className="text-sm h-full">
                     <ul>
                         <li>Email address: {user.emailAddress}</li>
                         <li>Role: {user.role}</li>
