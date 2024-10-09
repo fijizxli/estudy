83ec339b316263ee1e41e843dcee1f7d52dfc619
Szabó Zoltán <szabozoltan6x@gmail.com>
2024-10-09 22:21:06 +0200

    divide minio endpoint into host and port

--- fileupload-api/main.go
+++ fileupload-api/main.go
@@ -198,13 +198,14 @@ func main() {
 		c.Next()
 	})
 
-	endpoint := os.Getenv("MINIO_ENDPOINT")
+	host := os.Getenv("MINIO_HOST")
+	port := os.Getenv("MINIO_PORT")
 	accessKeyID := os.Getenv("MINIO_ACCESS_KEY_ID")
 	secretAccessKey := os.Getenv("MINIO_SECRET_ACCESS_KEY")
 
 	useSSL := false
 
-	minioClient, err := minio.New(endpoint, &minio.Options{
+	minioClient, err := minio.New(host+":"+port, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
 		Secure: useSSL,
 	})
