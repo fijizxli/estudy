acc57328193368de07396ea8a977cba0a44065b8
Szabó Zoltán <szabozoltan6x@gmail.com>
2024-10-10 21:50:23 +0200

    add env variables + containerize dotnet app

--- estudyRatings/Program.cs
+++ estudyRatings/Program.cs
@@ -1,13 +1,17 @@
 using estudyRatings.Components;
 using MongoDB.Driver;
 
-//var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
-var connectionString = "mongodb://root:example@localhost:27017/ratings?authSource=admin";
+var mongo_user = Environment.GetEnvironmentVariable("MONGO_USER");
+var mongo_password = Environment.GetEnvironmentVariable("MONGO_PASSWORD");
+var mongo_host = Environment.GetEnvironmentVariable("MONGO_HOST");
+var mongo_port = Environment.GetEnvironmentVariable("MONGO_PORT");
+var mongo_db = Environment.GetEnvironmentVariable("MONGO_DB");
+var connectionString = "mongodb://"+mongo_user+":"+mongo_password+"@"+mongo_host+":"+mongo_port+"/"+mongo_db+"?authSource=admin";
 if (connectionString == null)
 {
     Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, " +
-                      "see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string"
-                      );
+                    "see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string"
+                    );
     Environment.Exit(0);
 }
 MongoClient client = new MongoClient(connectionString);
