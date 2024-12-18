ba35eea811c44a0e6f38d5603b8624662f5df9c4
Zoltan Szabo <szabozoltan6x@gmail.com>
2024-12-18 12:52:14 +0100

    remove unused import

--- estudy-frontend/src/components/ErrorMessage.tsx
+++ estudy-frontend/src/components/ErrorMessage.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-
-function ErrorMessage(props) {
+function ErrorMessage(props: ErrorMessageProps) {
     return (
         <div className="errorMessage">{props.text}</div>
     )
